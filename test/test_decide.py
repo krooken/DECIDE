39410d2de0a0bd7b1c81e196f83a457335cf8994
Jonas Krook <jonas.krook@zenuity.com>
2019-02-07 16:56:24 +0100

    ADD: Creating test case for all false in the PUV vector
    Testing that the Final Unlocking Vector's elements all are set to true and a launch decision is issued if no Launch Interceptor Criteria can hold back launch.
    The test is failing right now since the DECIDE() function has a trivial implementation.

--- test/test_decide.py
+++ test/test_decide.py
@@ -35,3 +35,35 @@ class DecideTestCase(unittest.TestCase):
         puv = np.full(15, True)
 
         self.assertFalse(decide.decide(numpoints, points, parameters, lcm, puv))
+
+    def test_fuv_bypass(self):
+        # Test that setting all values in the PUM (Preliminary Unlocking Vector) to false (i.e. bypass checks) results
+        # in a launch decision.
+
+        numpoints = 3
+        points = np.array([(0.0,0.0), (1.0, 0.0), (2.0, 0.0)])
+        parameters = {
+            'length1': 10.0,
+            'radius1': 10.0,
+            'epsilon': math.pi/2.0,
+            'area1': 10.0,
+            'q_pts': 2,
+            'quads': 1,
+            'n_pts': 3,
+            'dist': 10.0,
+            'k_pts': 1,
+            'a_pts': 1,
+            'b_pts': 1,
+            'c_pts': 1,
+            'd_pts': 1,
+            'e_pts': 1,
+            'f_pts': 1,
+            'g_pts': 1,
+            'k_pts': 1,
+            'length2': 0.0,
+            'area2': 1.0
+        }
+        lcm = np.full((15,15), 1)
+        puv = np.full(15, False)
+
+        self.assertTrue(decide.decide(numpoints, points, parameters, lcm, puv))
