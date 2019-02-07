5fac82d898f9020fd24bc01a7604a9dd98cbf7d4
Jonas Krook <jonas.krook@zenuity.com>
2019-02-07 16:33:15 +0100

    ADD: Creating negative test
    Creating a test where the inputs to decide do not fulfill the criteria for launching, so decide should respond false.

---
+++ test/test_decide.py
@@ -0,0 +1,37 @@
+import unittest
+import decide
+from decide import decide
+import numpy as np
+import math
+
+class DecideTestCase(unittest.TestCase):
+
+    def test_negative_decision(self):
+        # Test that an input which does not fulfill the conditions will give a negative answer.
+        numpoints = 3
+        points = np.array([(0.0,0.0), (1.0, 0.0), (2.0, 0.0)])
+        parameters = {
+            'length1': 10.0,
+            'radius1': 10.0,
+            'epsilon': math.pi/2.0,
+            'area1': 10.0,
+            'q_pts': 2,
+            'quads': 1,
+            'n_pts': 3,
+            'dist': 10.0,
+            'k_pts': 1,
+            'a_pts': 1,
+            'b_pts': 1,
+            'c_pts': 1,
+            'd_pts': 1,
+            'e_pts': 1,
+            'f_pts': 1,
+            'g_pts': 1,
+            'k_pts': 1,
+            'length2': 0.0,
+            'area2': 1.0
+        }
+        lcm = np.zeros(15) + 3
+        puv = np.full(15, True)
+
+        self.assertFalse(decide.decide(numpoints, points, parameters, lcm, puv))
