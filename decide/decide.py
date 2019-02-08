3b26e227428643bbad09b62a1c9b6d4215eb8ffd
Jonas Krook <jonas.krook@zenuity.com>
2019-02-08 15:01:41 +0100

    ADD: Implementing code for LIC 0
    LIC 0 is true if two consecutive points are further apart than length1. Save that result in cmv. Then the final unlocking vector is calculated depending on how the LIC's shall be combined given in lcm, and whether LIC's should be bypassed given in puv.

--- decide/decide.py
+++ decide/decide.py
@@ -1,3 +1,5 @@
+import numpy as np
+
 def decide(
         numpoints,
         points,
@@ -6,8 +8,35 @@ def decide(
         puv
 ):
 
+    cmv = np.full(15, False)
+
+    for i in range(len(points)-1):
+        if (points[i+1][0] - points[i][0])**2 + (points[i+1][1] - points[i][1])**2 > parameters['length1']**2:
+            cmv[0] = True
+
+    pum = np.full((15,15), False)
+
+    for row in range(15):
+        for col in range(15):
+            if lcm[row,col] == 1:
+                pum[row,col] = cmv[row] and cmv[col]
+            elif lcm[row,col] == 2:
+                pum[row,col] = cmv[row] or cmv[col]
+            else:
+                pum[row,col] = True
+
+    fuv = np.full(15, False)
+    for row in range(15):
+        if puv[row]:
+            r = True
+            for col in range(15):
+                r &= pum[row,col]
+            fuv[row] = r
+        else:
+            fuv[row] = True
+
     result = True
-    for preliminary_unlocking_element in puv:
-        result &= not preliminary_unlocking_element
+    for final_unlocking_element in fuv:
+        result &= final_unlocking_element
 
     return result
