02f0e645fadb5a596ae27b10d39de802bb7b3459
Jonas Krook <jonas.krook@zenuity.com>
2019-02-08 13:58:40 +0100

    REFACTOR: Move statements into the setUp method
    Instead of setting up the inputs in every test method, the inputs are set up in a dedicated method. This way there is less copy-pasting and easier maintenance. It is also easier to see how the input is different from the default in every test case.

--- test/test_decide.py
+++ test/test_decide.py
@@ -6,11 +6,12 @@ import math
 
 class DecideTestCase(unittest.TestCase):
 
-    def test_negative_decision(self):
-        # Test that an input which does not fulfill the conditions will give a negative answer.
-        numpoints = 3
-        points = np.array([(0.0,0.0), (1.0, 0.0), (2.0, 0.0)])
-        parameters = {
+    def setUp(self):
+        # Set up default input data to the function under test in order to reduce the need for copy-pasting and
+        # maintenence.
+        self.numpoints = 3
+        self.points = np.array([(0.0,0.0), (1.0, 0.0), (2.0, 0.0)])
+        self.parameters = {
             'length1': 10.0,
             'radius1': 10.0,
             'epsilon': math.pi/2.0,
@@ -31,39 +32,19 @@ class DecideTestCase(unittest.TestCase):
             'length2': 0.0,
             'area2': 1.0
         }
-        lcm = np.full((15,15), 1)
-        puv = np.full(15, True)
+        self.lcm = np.full((15,15), 1)
+        self.puv = np.full(15, True)
 
-        self.assertFalse(decide.decide(numpoints, points, parameters, lcm, puv))
+    def test_negative_decision(self):
+        # Test that an input which does not fulfill the conditions will give a negative answer.
+
+
+        self.assertFalse(decide.decide(self.numpoints, self.points, self.parameters, self.lcm, self.puv))
 
     def test_fuv_bypass(self):
         # Test that setting all values in the PUV (Preliminary Unlocking Vector) to false (i.e. bypass checks) results
         # in a launch decision.
 
-        numpoints = 3
-        points = np.array([(0.0,0.0), (1.0, 0.0), (2.0, 0.0)])
-        parameters = {
-            'length1': 10.0,
-            'radius1': 10.0,
-            'epsilon': math.pi/2.0,
-            'area1': 10.0,
-            'q_pts': 2,
-            'quads': 1,
-            'n_pts': 3,
-            'dist': 10.0,
-            'k_pts': 1,
-            'a_pts': 1,
-            'b_pts': 1,
-            'c_pts': 1,
-            'd_pts': 1,
-            'e_pts': 1,
-            'f_pts': 1,
-            'g_pts': 1,
-            'k_pts': 1,
-            'length2': 0.0,
-            'area2': 1.0
-        }
-        lcm = np.full((15,15), 1)
-        puv = np.full(15, False)
+        self.puv = np.full(15, False)
 
-        self.assertTrue(decide.decide(numpoints, points, parameters, lcm, puv))
+        self.assertTrue(decide.decide(self.numpoints, self.points, self.parameters, self.lcm, self.puv))
