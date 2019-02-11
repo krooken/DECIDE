ca5d50b82a959d8987dd04a3b9c6c779b6e4756c
Jonas Krook <jonas.krook@zenuity.com>
2019-02-11 12:41:20 +0100

    DOC: Fixing typo

--- decide/decide.py
+++ decide/decide.py
@@ -66,5 +66,5 @@ def check_lic0(points, parameters):
 
 def point_distance(point1,point2):
 
-    # Calculate the euclidian distance between two points
+    # Calculate the euclidean distance between two points
     return ( (point1[0] - point2[0])**2 + (point1[1] - point2[1])**2 )**0.5
